75f52977f599a618aa40507ecd75d54620329908
Tien <vuductien2908@gmail.com>
2022-06-13 08:33:02 +0700

    Refactor code

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ const morgan = require("morgan");
 const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
+const route = require("./routes");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // HTTP logger
 app.use(morgan("combined"));
@@ -15,13 +18,8 @@ app.engine("hbs", engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+// Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
