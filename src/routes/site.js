04bad6dea30363b17bed12dfcab66aa9d20e9f0d
Tien <vuductien2908@gmail.com>
2022-06-17 13:31:40 +0700

    CRUD: vs1

--- src/routes/site.js
+++ src/routes/site.js
@@ -2,10 +2,14 @@ const express = require("express");
 const router = express.Router();
 const siteController = require("../app/controllers/SiteController");
 
-router.get("/search", siteController.search);
 router.post("/", siteController.find);
-router.get("/addPhone", siteController.form);
+router.get("/search", siteController.search);
 router.post("/addPhone", siteController.create);
+router.get("/addPhone", siteController.form);
+router.get("/editPhone/:id", siteController.edit);
+router.post("/editPhone/:id", siteController.update);
+router.get("/viewProduct/:id", siteController.view);
+router.get("/:id", siteController.delete);
 router.get("/", siteController.home);
 
 module.exports = router;
